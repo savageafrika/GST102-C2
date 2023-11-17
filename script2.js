cb0b7e510536c1c1f71533cfeec65317d603bd45
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-17 03:21:51 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,491 @@
+
+
+
+
+ 
+
+   //Questions array
+
+   let questions = [
+
+    {
+
+         number:1,
+
+         question: "___ is the study of the shapes and behavior of sentences",
+
+         answer : "Syntax",
+
+         options : [
+
+                          "Syntax",
+
+                          "Morphology",
+
+                          "Semantics",
+
+                          "Linguistics"
+
+                        ]
+
+    
+
+    },
+
+    {
+
+         number:2,
+
+         question: "___ is the basic unit of thought",
+
+         answer : "Sentence",
+
+         options : [
+
+                          "Phrase",
+
+                          "Sentence",
+
+                          "Clause",
+
+                          "Fragment"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:3,
+
+         question: "The information given about the subject in the sentence is called",
+
+         answer : "Predicate",
+
+         options : [
+
+                          "Fragment",
+
+                          "Modifier",
+
+                          "Complement",
+
+                          "Predicate"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:4,
+
+         question: "My parents live in Abuja. This is __ type of sentence",
+
+         answer : "Simple Sentence",
+
+         options : [
+
+                          "Simple Sentence",
+
+                          "Compound complex sentence",
+
+                          "Compound sentence",
+
+                          "Complex sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:5,
+
+         question: "I knew that James was ill, but I didn't know he suffered from smallpox. What type of sentence is this",
+
+         answer : "Compound complex sentence",
+
+         options : [
+
+                          "Simple Sentence",
+
+                          "Compound sentence",
+
+                          "Complex sentence",
+
+                          "Compound complex sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:6,
+
+         question: "I will buy you a car, if you pass your exam. This is what type of sentence",
+
+         answer : "Complex sentence",
+
+         options : [
+
+                          "Simple Sentence",
+
+                          "Compound sentence",
+
+                          "Complex sentence",
+
+                          "Compound complex sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:7,
+
+         question: "The boy came in and she shut the door. What type of sentence is this",
+
+         answer : "Compound sentence",
+
+         options : [
+
+                          "Simple Sentence",
+
+                          "Compound sentence",
+
+                          "Complex sentence",
+
+                          "Compound complex sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:8,
+
+         question: "A compound sentence is made up of two simple sentence is joined by ___",
+
+         answer : "Coordinating conjunction",
+
+         options : [
+
+                          "Coordinating conjunction",
+
+                          "Determiner",
+
+                          "Post quantifier",
+
+                          "Subordinate conjunction"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:9,
+
+         question: "Which of these is not a sentence type according to function",
+
+         answer : "Expository sentence",
+
+         options : [
+
+                          "Declarative sentence",
+
+                          "Interrogative sentence",
+
+                          "Expository sentence",
+
+                          "Imperative sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:10,
+
+         question: "__ is used to issue out command",
+
+         answer : "Imperative sentence",
+
+         options : [
+
+                          "Declarative sentence",
+
+                          "Interrogative sentence",
+
+                          "Expository sentence",
+
+                          "Imperative sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:11,
+
+         question: "Interrogative sentence is written in ___",
+
+         answer : "Inverted order",
+
+         options : [
+
+                          "Assertive form",
+
+                          "Inverted order",
+
+                          "Syntactic category",
+
+                          "Complex sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:12,
+
+         question: "___ type of sentence states a fact or makes an assertion",
+
+         answer : "Declarative sentence",
+
+         options : [
+
+                          "Declarative sentence",
+
+                          "Interrogative sentence",
+
+                          "Expository sentence",
+
+                          "Imperative sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:13,
+
+         question: "___ suggest that the speaker is irritated",
+
+         answer : "Forceful imperative",
+
+         options : [
+
+                          "Negative imperative",
+
+                          "Mild imperative",
+
+                          "Single word imperative",
+
+                          "Forceful imperative"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:14,
+
+         question: "An incomplete sentence is called __",
+
+         answer : "Sentence fragment",
+
+         options : [
+
+                          "Phrase",
+
+                          "Sentence fragment",
+
+                          "Clause",
+
+                          "Determiners"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:15,
+
+         question: "___ emphasizes what should not be done",
+
+         answer : "Negative imperative",
+
+         options : [
+
+                          "Negative imperative",
+
+                          "Mild imperative",
+
+                          "Single word imperative",
+
+                          "Forceful imperative"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:16,
+
+         question: "You made it! This is __ type of sentence",
+
+         answer : "Exclamatory sentence",
+
+         options : [
+
+                          "Declarative sentence",
+
+                          "Interrogative sentence",
+
+                          "Exclamatory sentence",
+
+                          "Imperative sentence"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:17,
+
+         question: "Complex sentences are joined with a ___",
+
+         answer : "Subordinate conjunction",
+
+         options : [
+
+                          "Coordinating conjunction",
+
+                          "Determiner",
+
+                          "Post quantifier",
+
+                          "Subordinate conjunction"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:18,
+
+         question: "Shut the door! What type of imperative is this",
+
+         answer : "Single word imperative",
+
+         options : [
+
+                          "Negative imperative",
+
+                          "Mild imperative",
+
+                          "Single word imperative",
+
+                          "Forceful imperative"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:19,
+
+         question: "___ imperatives have the implied subject ‘you`",
+
+         answer : "Single word imperative",
+
+         options : [
+
+                          "Negative imperative",
+
+                          "Mild imperatives",
+
+                          "Single word imperative",
+
+                          "Forceful imperative"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:20,
+
+         question: "Although it started to rain, we still made it to the revival. This is what type of fragment",
+
+         answer : "Dependent word fragment",
+
+         options : [
+
+                          "Dependent word fragment",
+
+                          "ING fragment",
+
+                          "Single word fragment",
+
+                          "Group of words without finite verbs"  
+
+                          ]
+
+    
+
+    }
+
+     ];   
